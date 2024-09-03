ba98a5be0e9c360b8cbc81cab4e7e4a2a2d80337
Ness <bur>
2024-09-03 17:05:15 +0300

    Добавьте файлы проекта.

---
+++ Lesson2_2/Lesson2_2.cpp
@@ -0,0 +1,23 @@
+﻿
+
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+    int a = 42;
+    int b = 153;
+    int c;
+    cout << "a: " << a << "\n"; 
+    cout << "b: " << b << "\n";
+
+    c = a;
+    a = b;
+    b = c;
+
+    cout << "a: " << a << "\n"; 
+    cout << "b: " << b << "\n";
+
+}
+
